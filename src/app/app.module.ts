6132ea5045c09af78520c526741a9ef3b7c30145
Sandro Costa <smachadoc@minsait.com>
2024-01-24 18:15:03 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './features/home/home.component';
+import { TesteComponent } from './features/teste/teste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
